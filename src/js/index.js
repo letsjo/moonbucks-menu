797f7cde5a9fecc73d657624624580b6d1ae3ce7
letsjo <hyunoh.jo@gmail.com>
2023-02-28 21:27:37 +0900

    Feat: 입력받은 메뉴 ul 태그 내부 삽입 및 데이터 쌓기 완료
    insertAdjacentHTML 메서드를 통해서 beforeend 옵션으로 데이터 쌓기 완료

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 // ### ⬜ TODO 메뉴 추가
 
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
-// - [ ] 메뉴의 이름 입력값이 빈 값이라면 추가되지 않는다.
-// - [ ] 추가되는 메뉴의 아래 마크업은
+// - [x] 추가되는 메뉴의 아래 마크업은
 //       `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입
 //       해야 한다.
+// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [ ] 메뉴의 이름 입력값이 빈 값이라면 추가되지 않는다.
 // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
@@ -21,7 +21,36 @@ function App() {
   // 메뉴의 이름을 입력 받는건
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      console.log(document.querySelector('#espresso-menu-name').value);
+      const espressoMenuName = $('#espresso-menu-name').value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `<li class='menu-list-item d-flex items-center py-2'>
+                  <span class='w-100 pl-2 menu-name'>${espressoMenuName}</span>
+                  <button
+                    type='button'
+                    class='bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button'
+                  >
+                    수정
+                  </button>
+                  <button
+                    type='button'
+                    class='bg-gray-50 text-gray-500 text-sm menu-remove-button'
+                  >
+                    삭제
+                  </button>
+                </li>`
+      }
+      // <!-- beforebegin -->
+      // <p>
+      // <!-- afterbegin -->
+      // foo
+      // <!-- beforeend -->
+      // </p>
+      // <!-- afterend -->
+      $('#espresso-menu-list')
+        .insertAdjacentHTML(
+          'beforeend',
+          menuItemTemplate(espressoMenuName)
+        )
     }
   });
 }
