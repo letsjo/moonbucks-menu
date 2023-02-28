2556d0ea0ead6dcd33e609e3420a41f97afc8986
letsjo <hyunoh.jo@gmail.com>
2023-02-28 21:58:28 +0900

    Feat: 확인 버튼을 클릭하면 메뉴 추가
    addMenuName 함수를 통해서 Menu를 추가하는 기능을 분리
    addEventListener 의 click 옵션으로 이벤트를 받아 addMenuName 실행

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 // ### ⬜ TODO 메뉴 추가
 
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [x] 추가되는 메뉴의 아래 마크업은
 //       `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입
 //       해야 한다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
-// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 
 const $ = (selector) => document.querySelector(selector);
 
@@ -17,13 +17,8 @@ function App() {
     e.preventDefault();
   })
 
-  // 메뉴의 이름을 입력 받는건
-  $('#espresso-menu-name').addEventListener('keypress', (e) => {
-    // 엔터키를 누르지 않으면 return 해준다.
-    if (e.key !== 'Enter') {
-      return;
-    }
-    // 엔터키를 누르면, 입력값을 받아온다.
+  // 메뉴 추가시 유효성 검사
+  const addMenuName = () => {
     if ($('#espresso-menu-name').value === '') {
       alert('값을 입력해주세요');
       return;
@@ -57,6 +52,20 @@ function App() {
 
     // 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
     $('#espresso-menu-name').value = '';
+  }
+
+  // 확인 버튼을 누르면 메뉴를 추가한다.
+  $('#espresso-menu-submit-button').addEventListener('click', () => {
+    addMenuName();
+  })
+
+  // 메뉴의 이름을 입력 받는건
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    // 엔터키를 누르지 않으면 return 해준다.
+    if (e.key !== 'Enter') {
+      return;
+    }
+    addMenuName();
   }
   );
 }
