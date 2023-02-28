40a75ab4610294027eef629b279c3ca3734a18c1
letsjo <hyunoh.jo@gmail.com>
2023-03-01 00:30:24 +0900

    Feat: rander 함수 분리 및 menu 값 parse 처리

--- src/js/index.js
+++ src/js/index.js
@@ -5,29 +5,21 @@ const store = {
     localStorage.setItem('menu', JSON.stringify(menu));
   },
   getLocalStorage() {
-    localStorage.getItem('menu');
+    return JSON.parse(localStorage.getItem('menu'));
   }
 }
 
 function App() {
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
   this.menu = [];
-
-  const updateMenuCount = () => {
-    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
-    $('.menu-count').innerText = `총 ${menuCount}개`
+  this.init = () => {
+    if (store.getLocalStorage().length > 1) {
+      this.menu = store.getLocalStorage();
+    }
+    render();
   }
 
-  const addMenuName = () => {
-
-    if ($('#espresso-menu-name').value === '') {
-      alert('값을 입력해주세요');
-      return;
-    }
-
-    const espressoMenuName = $('#espresso-menu-name').value;
-    this.menu.push({ name: espressoMenuName });
-    store.setLocalStorage(this.menu);
+  const render = () => {
     const template = this.menu.map((menuItem, index) => {
       return `<li data-menu-id="${index}" class='menu-list-item d-flex items-center py-2'>
                 <span class='w-100 pl-2 menu-name'>${menuItem.name}</span>
@@ -49,6 +41,24 @@ function App() {
     $('#espresso-menu-list').innerHTML = template;
 
     updateMenuCount();
+  }
+
+  const updateMenuCount = () => {
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerText = `총 ${menuCount}개`
+  }
+
+  const addMenuName = () => {
+
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해주세요');
+      return;
+    }
+
+    const espressoMenuName = $('#espresso-menu-name').value;
+    this.menu.push({ name: espressoMenuName });
+    store.setLocalStorage(this.menu);
+    render();
 
     $('#espresso-menu-name').value = '';
   }
@@ -112,4 +122,5 @@ function App() {
   );
 }
 
-const a = new App();
+const app = new App();
+app.init();
