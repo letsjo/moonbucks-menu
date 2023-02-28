1197593a3cdb03a4b0dd62b737bd7acfca353579
letsjo <hyunoh.jo@gmail.com>
2023-03-01 01:42:09 +0900

    Feat: Get each category name from click event

--- src/js/index.js
+++ src/js/index.js
@@ -120,6 +120,14 @@ function App() {
     addMenuName();
   }
   );
+
+  $('nav').addEventListener('click', (e) => {
+    const isCategoryButton = e.target.classList.contains('cafe-category-name');
+    if (isCategoryButton) {
+      const categoryName = e.target.dataset.categoryName;
+      console.log(categoryName);
+    }
+  });
 }
 
 const app = new App();
