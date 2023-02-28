28901508319819fc525e8b486cdf250f1c567e03
letsjo <hyunoh.jo@gmail.com>
2023-02-28 22:34:35 +0900

    Feat: 빈값이거나 취소버튼을 누르면 수정되지 않는 기능 추가
    취소버튼을 누를 경우 prompt 반환값이 null로 되기 때문에 수정하지 않고 return 처리
    빈값으로 확인 버튼을 누를 경우 ''로 되기 때문에 alert 으로 피드백 하고 수정하지 않고 return 처리

--- src/js/index.js
+++ src/js/index.js
@@ -11,12 +11,21 @@ const $ = (selector) => document.querySelector(selector);
 function App() {
   $('#espresso-menu-list').addEventListener('click', (e) => {
     if (e.target.classList.contains('menu-edit-button')) {
-      const menuName = e.target
-        .closest('li')
-        .querySelector('.menu-name').innerText;
-      prompt('메뉴명을 수정하세요', menuName);
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요',
+        $menuName.innerText
+      );
+      if (updatedMenuName === null) {
+        return;
+      }
+      if (updatedMenuName === '') {
+        alert('값을 입력해주세요');
+        return;
+      }
+      $menuName.innerText = updatedMenuName;
     }
-  })
+  });
 
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
