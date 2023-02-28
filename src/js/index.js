6c62724bbdd287522d7668a08d9c3e06008898ac
letsjo <hyunoh.jo@gmail.com>
2023-02-28 21:11:54 +0900

    Refactor: querySelector 유틸 함수 생성
    반복되는 코드 삭제.
    관용적으로 $ 표시를 querySelector 로 줄여서 사용.

--- src/js/index.js
+++ src/js/index.js
@@ -10,22 +10,20 @@
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form태그가 자동으로 전송되는 것을 막아준다
-  document
-    .querySelector('#espresso-menu-form')
-    .addEventListener('submit', (e) => {
-      e.preventDefault();
-    })
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  })
 
   // 메뉴의 이름을 입력 받는건
-  document
-    .querySelector('#espresso-menu-name')
-    .addEventListener('keypress', (e) => {
-      if (e.key === 'Enter') {
-        console.log(document.querySelector('#espresso-menu-name').value);
-      }
-    });
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      console.log(document.querySelector('#espresso-menu-name').value);
+    }
+  });
 }
 
 App();
