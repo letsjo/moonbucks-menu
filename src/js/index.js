6cd44a2c833fc39f0eea37023547115273fb5875
letsjo <hyunoh.jo@gmail.com>
2023-02-28 21:35:10 +0900

    Feat: 입력받은 메뉴 count 후 상단에 표시
    querySelectorAll 의 length를 통해서 li 태그 갯수로 추가된 메뉴 수를 구함
    innerText 로 내부 텍스트를 변경해서 갯수를 표시함.

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,9 @@
 // - [x] 추가되는 메뉴의 아래 마크업은
 //       `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입
 //       해야 한다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [ ] 메뉴의 이름 입력값이 빈 값이라면 추가되지 않는다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
@@ -39,18 +39,14 @@ function App() {
                   </button>
                 </li>`
       }
-      // <!-- beforebegin -->
-      // <p>
-      // <!-- afterbegin -->
-      // foo
-      // <!-- beforeend -->
-      // </p>
-      // <!-- afterend -->
-      $('#espresso-menu-list')
-        .insertAdjacentHTML(
-          'beforeend',
-          menuItemTemplate(espressoMenuName)
-        )
+      $('#espresso-menu-list').insertAdjacentHTML(
+        'beforeend',
+        menuItemTemplate(espressoMenuName)
+      );
+
+      //const 변수 = li 갯수를 카운팅
+      const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+      $('.menu-count').innerText = `총 ${menuCount}개`
     }
   });
 }
