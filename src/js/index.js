695a907b9ac4f2018f093fac85a0ce59701707f9
letsjo <hyunoh.jo@gmail.com>
2023-03-01 15:11:02 +0900

    Feat: fetch 를 통한 API 요청 및 async await을 통한 동기처리

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import { $ } from './utils/dom.js';
 import store from './store/index.js';
 
+const BASE_URL = 'http://localhost:3000/api';
+
 function App() {
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
   this.menu = {
@@ -22,11 +24,15 @@ function App() {
   };
 
   const render = () => {
-    const template = this.menu[this.currentCategory]
+    const template = this.menu[
+      this.currentCategory
+    ]
       .map((menuItem, index) => {
         return `<li data-menu-id="${index}" class='menu-list-item d-flex items-center py-2'>
                 <span class='w-100 pl-2 menu-name ${
-                  menuItem.soldOut ? 'sold-out' : ''
+                  menuItem.soldOut
+                    ? 'sold-out'
+                    : ''
                 }'>${menuItem.name}</span>
                 <button
                   type='button'
@@ -56,26 +62,58 @@ function App() {
   };
 
   const updateMenuCount = () => {
-    const menuCount = this.menu[this.currentCategory].length;
-    $('.menu-count').innerText = `총 ${menuCount}개`;
+    const menuCount =
+      this.menu[this.currentCategory].length;
+    $(
+      '.menu-count',
+    ).innerText = `총 ${menuCount}개`;
   };
 
-  const addMenuName = () => {
+  const addMenuName = async () => {
     if ($('#menu-name').value === '') {
       alert('값을 입력해주세요');
       return;
     }
 
     const menuName = $('#menu-name').value;
-    this.menu[this.currentCategory].push({ name: menuName });
-    store.setLocalStorage(this.menu);
-    render();
 
-    $('#menu-name').value = '';
+    await fetch(
+      `${BASE_URL}/category/${this.currentCategory}/menu`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: menuName }),
+      },
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+
+    await fetch(
+      `${BASE_URL}/category/${this.currentCategory}/menu`,
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+
+    // this.menu[this.currentCategory].push({ name: menuName });
+    // store.setLocalStorage(this.menu);
+    // render();
+
+    // $('#menu-name').value = '';
   };
 
   const updateMenuName = (e) => {
-    const menuId = e.target.closest('li').dataset.menuId;
+    const menuId =
+      e.target.closest('li').dataset.menuId;
     const $menuName = e.target
       .closest('li')
       .querySelector('.menu-name');
@@ -94,65 +132,100 @@ function App() {
       return;
     }
 
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
+    this.menu[this.currentCategory][menuId].name =
+      updatedMenuName;
     store.setLocalStorage(this.menu);
     render();
   };
 
   const removeMenuName = (e) => {
     if (confirm('정말 삭제하시겠습니까?')) {
-      const menuId = e.target.closest('li').dataset.menuId;
-      this.menu[this.currentCategory].splice(menuId, 1);
+      const menuId =
+        e.target.closest('li').dataset.menuId;
+      this.menu[this.currentCategory].splice(
+        menuId,
+        1,
+      );
       store.setLocalStorage(this.menu);
       render();
     }
   };
 
   const soldOutMenu = (e) => {
-    const menuId = e.target.closest('li').dataset.menuId;
-    this.menu[this.currentCategory][menuId].soldOut =
-      !this.menu[this.currentCategory][menuId].soldOut;
+    const menuId =
+      e.target.closest('li').dataset.menuId;
+    this.menu[this.currentCategory][
+      menuId
+    ].soldOut =
+      !this.menu[this.currentCategory][menuId]
+        .soldOut;
     store.setLocalStorage(this.menu);
     render();
   };
 
   const initEventListeners = () => {
-    $('#menu-list').addEventListener('click', (e) => {
-      if (e.target.classList.contains('menu-edit-button')) {
-        updateMenuName(e);
-        return;
-      }
+    $('#menu-list').addEventListener(
+      'click',
+      (e) => {
+        if (
+          e.target.classList.contains(
+            'menu-edit-button',
+          )
+        ) {
+          updateMenuName(e);
+          return;
+        }
 
-      if (e.target.classList.contains('menu-remove-button')) {
-        removeMenuName(e);
-        return;
-      }
+        if (
+          e.target.classList.contains(
+            'menu-remove-button',
+          )
+        ) {
+          removeMenuName(e);
+          return;
+        }
 
-      if (e.target.classList.contains('menu-sold-out-button')) {
-        soldOutMenu(e);
-        return;
-      }
-    });
+        if (
+          e.target.classList.contains(
+            'menu-sold-out-button',
+          )
+        ) {
+          soldOutMenu(e);
+          return;
+        }
+      },
+    );
 
-    $('#menu-form').addEventListener('submit', (e) => {
-      e.preventDefault();
-    });
+    $('#menu-form').addEventListener(
+      'submit',
+      (e) => {
+        e.preventDefault();
+      },
+    );
 
-    $('#menu-submit-button').addEventListener('click', addMenuName);
+    $('#menu-submit-button').addEventListener(
+      'click',
+      addMenuName,
+    );
 
-    $('#menu-name').addEventListener('keypress', (e) => {
-      if (e.key !== 'Enter') {
-        return;
-      }
-      addMenuName();
-    });
+    $('#menu-name').addEventListener(
+      'keypress',
+      (e) => {
+        if (e.key !== 'Enter') {
+          return;
+        }
+        addMenuName();
+      },
+    );
 
     $('nav').addEventListener('click', (e) => {
-      const isCategoryButton = e.target.classList.contains(
-        'cafe-category-name',
-      );
+      const isCategoryButton =
+        e.target.classList.contains(
+          'cafe-category-name',
+        );
       if (isCategoryButton) {
-        const categoryName = e.target.dataset.categoryName;
+        const categoryName =
+          e.target.dataset.categoryName;
         this.currentCategory = categoryName;
         $(
           '#category-title',
